82a0eeb6a6ab0ab2ae18d2480ed5e4004ed3b35a
Yadnyesh001 <yadnyeshbadgujar2004@gmail.com>
2025-02-02 23:15:30 +0530

    mailtrap setup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+import { sendVerificationEmail, sendWelcomeEmail } from '../mailtrap/emails.js';
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
 
   try {
     if(!email || !password || !name) {
-      return res.status(400).json({message: 'Email and password are required'});
+      return res.status(400).json({message: 'All fields are required'});
     }
     
     const userAlreadyExists = await User.findOne({ email });
@@ -31,6 +32,8 @@ export const signup = async (req, res) => {
 
     generateTokenAndSetCookie(res, user._id);
 
+    sendVerificationEmail(user.email, verificationToken);
+
     res.status(201).json({
       success: true, 
       message: 'User created successfully',
@@ -42,6 +45,41 @@ export const signup = async (req, res) => {
 
   } catch (error) {
     console.error('Signup error:', error);
+    res.status(500).json({success: false, message: 'Internal server error'});
+  }
+}
+
+export const verifyEmail = async (req, res) => {  
+  const {code} = req.body;
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() }
+    });
+
+    if(!user) {
+      return res.status(400).json({success: false, message: 'Invalid or expired verification code'});
+    }
+
+    user.isVerified = true;
+    user.verificationToken = undefined;
+    user.verificationTokenExpiresAt = undefined;
+
+    await user.save();
+
+    await sendWelcomeEmail(user.email, user.name);
+
+    res.status(200).json({
+      success: true, 
+      message: 'Email verified successfully',
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+    
+  } catch (error) {
+    console.error('Verify email error:', error);
     res.status(500).json({success: false, message: 'Internal server error'});
   }
 }
